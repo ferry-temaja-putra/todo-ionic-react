6bea1d7e712ce5f166a18f6c6b2f960607e8db3c
Ferry Temaja Putra <Ferry_TemajaPutra@technologyonecorp.com>
2021-12-29 20:10:14 +0800

    Update default UI.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,64 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { useEffect, useRef, useState } from 'react';
+import { v1 as uuidv1 } from 'uuid';
 import './Home.css';
 
+interface Task {
+  id: string;
+  date: Date;
+  content: string;
+};
+
 const Home: React.FC = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [taskContent, setTaskContent] = useState<string>('');
+  const inputTaskRef = useRef<any>(null);
+
+  useEffect(() => {
+    inputTaskRef.current.setFocus();
+  });
+  
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>React Todo</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <IonGrid>
+          <IonRow class='ion-justify-content-center'>
+            <IonCol sizeXs='12' sizeLg='6' sizeXl='4'>
+              <IonItem class='ion-margin-bottom'>
+                <IonLabel position='floating'>Enter the task</IonLabel>
+                <IonInput ref={inputTaskRef} value={taskContent} onIonChange={(e) => {
+                  let inputValue = '';
+                  if (e.detail.value) {
+                    inputValue  = e.detail.value;
+                  }
+                  setTaskContent(inputValue);
+                }} />
+              </IonItem>
+              <IonButton expand='block' onClick={() => {
+                if (!taskContent) {
+                  return;
+                }
+                const task: Task = {
+                  id: uuidv1(),
+                  date: new Date(),
+                  content: taskContent
+                };
+
+                const updatedTasks = tasks.concat(task);
+
+                setTasks(updatedTasks);
+                setTaskContent('');
+                inputTaskRef.current.setFocus();
+                console.log(tasks);
+              }}>Add</IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
